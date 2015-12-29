906c46cd14a76e00b76179a54a38cf87e443ccaa
tobspr <tobias.springer1@googlemail.com>
2015-12-29 20:52:57 +0100

    Fix wrong path, update scene

--- 01-Material-Demo/main.py
+++ 01-Material-Demo/main.py
@@ -36,8 +36,8 @@ class MainApp(ShowBase):
 
         # Insert the pipeline path to the system path, this is required to be
         # able to import the pipeline classes
-        # pipeline_path = "../../"
-        pipeline_path = "../../RenderPipeline/"
+        pipeline_path = "../../"
+        # pipeline_path = "../../RenderPipeline/"
         sys.path.insert(0, pipeline_path)
 
         from __init__ import RenderPipeline, SpotLight
