3d40d237c4bf6f8e608a9e8860f1eb60422327ad
tobspr <tobias.springer1@googlemail.com>
2016-01-03 16:36:11 +0100

    Update roaming ralph demo

--- 02-Roaming-Ralph/main.py
+++ 02-Roaming-Ralph/main.py
@@ -13,7 +13,7 @@ from panda3d.core import CollisionTraverser,CollisionNode
 from panda3d.core import CollisionHandlerQueue,CollisionRay
 from panda3d.core import Filename,AmbientLight,DirectionalLight
 from panda3d.core import PandaNode,NodePath,Camera,TextNode
-from panda3d.core import Vec3,Vec4,BitMask32, load_prc_file
+from panda3d.core import Vec3,Vec4,BitMask32, load_prc_file, load_prc_file_data
 from direct.gui.OnscreenText import OnscreenText
 from direct.actor.Actor import Actor
 from direct.showbase.ShowBase import ShowBase
@@ -35,15 +35,28 @@ class World(ShowBase):
 
     def __init__(self):
 
+        # Setup window size, title and so on
+        load_prc_file_data("", """
+        win-size 1920 1080
+        window-title Render Pipeline by tobspr 
+        icon-filename Data/GUI/icon.ico
+        """)
+
         # ------ Begin of render pipeline code ------
 
         # Insert the pipeline path to the system path, this is required to be
         # able to import the pipeline classes
         pipeline_path = "../../"
-        # pipeline_path = "../../RenderPipeline/"
+
+        # Just a special case for my development setup, so I don't accidentally
+        # commit a wrong path. You can remove this in your own programs.
+        if not os.path.isfile(os.path.join(pipeline_path, "setup.py")):
+            pipeline_path = "../../RenderPipeline/"
+
         sys.path.insert(0, pipeline_path)
 
-        from __init__ import RenderPipeline
+        from __init__ import RenderPipeline, SpotLight
+        from Code.Util.MovementController import MovementController
 
         self.render_pipeline = RenderPipeline(self)
         self.render_pipeline.create()
@@ -52,6 +65,11 @@ class World(ShowBase):
         self.render_pipeline.create_default_skybox()
 
         # ------ End of render pipeline code, thats it! ------
+
+
+        # Use a special effect for rendering the scene, this is because the
+        # roaming ralph model has no normals or valid materials
+        self.render_pipeline.set_effect(render, "scene-effect.yaml", {}, sort=50)
 
 
         
