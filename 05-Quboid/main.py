f79703f98ca8b1f55fceafc6b06814d569663d4f
tobspr <tobias.springer1@googlemail.com>
2016-01-17 13:37:12 +0100

    Update quboid sample

--- 05-Quboid/main.py
+++ 05-Quboid/main.py
@@ -5,6 +5,10 @@
 import os
 import sys
 
+print("This sample is currently broken! Please wait until it is fixed. You can "
+      "run it on your own risk by commenting the line below.")
+sys.exit(0)
+
 # Change to the current directory
 os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__))))
 
